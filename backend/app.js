e4a4d5189d225311d19cf289eed394277d2139e5
ekky2412 <ekky.raharjo@gmail.com>
2023-10-17 11:35:06 +0700

    init backend

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import express from "express";
+// import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import router from "./routes/route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+// Not have db yet
+// const DB_URL = process.env.DB_URL;
+
+// mongoose.connect(DB_URL);
+// const db = mongoose.connection;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
