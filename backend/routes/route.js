1b699fef3d5d0c62cae71970e76297605709e566
ekky2412 <ekky.raharjo@gmail.com>
2023-10-18 21:15:17 +0700

    add route plan

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,4 +1,15 @@
 import express from "express";
 const router = express.Router();
 
+// Admin
+// router.get("/user", getUsers);
+// router.get("/user/:id", getDetailUser);
+// router.post("/user", addUser);
+
+// User
+// router.post("/login/auth", addLoginAuth);
+// router.get("/journal", getUserJournal);
+// router.get("/journal/theme/:theme", getUserJournalTheme);
+// router.get("/journal/theme/:theme/:section", getUserJournalThemeSection);
+
 export default router;
