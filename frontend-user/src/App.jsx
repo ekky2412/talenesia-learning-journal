2f80f114adb9f27bdabae49ce2d48f5d6a285f83
muhammad fauzan <mhmmdfauzan296@gmail.com>
2023-10-21 23:56:05 +0700

    feat: add main page user

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -1,43 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-
 export default App
