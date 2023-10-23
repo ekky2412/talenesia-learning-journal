139a969f9915c26d0b0f3516935a8b0ea780f733
carlesoctav <carlesoctavianus@tuta.io>
2023-10-23 18:16:16 +0700

    renaming thingy

--- frontend-user/src/components/tema7/index.js
+++ frontend-user/src/components/tema7/index.js
@@ -1,7 +1,7 @@
 
-export { default as Header } from './header';
-export { default as Section1 } from './section1';
-export { default as Section2 } from './section2';
+export { default as Header } from './Header';
+export { default as Section1 } from './Section1';
+export { default as Section2 } from './Section2';
 export { default as Section3 } from './Section3';
 export { default as Section4 } from './Section4';
 export { default as Section5 } from './Section5';
